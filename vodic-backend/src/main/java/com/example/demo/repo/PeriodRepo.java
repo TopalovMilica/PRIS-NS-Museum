408de6282905b6d37acc76a6cb9b59a3bcb6f7cf
TopalovMilica <topalovm13@gmail.com>
2024-07-27 23:40:10 +0200

    Period CRUD

--- vodic-backend/src/main/java/com/example/demo/repo/PeriodRepo.java
+++ vodic-backend/src/main/java/com/example/demo/repo/PeriodRepo.java
@@ -1,9 +1,13 @@
 package com.example.demo.repo;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.model.Period;
 
 public interface PeriodRepo extends JpaRepository<Period, Integer> {
+	
+	Optional<Period> findbyNazivPocetakKraj(String naziv, int pocetak_perioda, int kraj_perioda);
 
 }
